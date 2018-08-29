2f76f832f7a217291eb8d2a167af8b83d8e0d202
andreasrmr <andreasrmr@hotmail.com>
2018-08-29 21:04:03 +0200

    .

---
+++ HelloWorld/Ex2HelloWorldApp2.java
@@ -0,0 +1,7 @@
+class HelloWorldApp2 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Display the string.
+    }
+}
+
+//https://docs.oracle.com/javase/tutorial/getStarted/QandE/questions.html
