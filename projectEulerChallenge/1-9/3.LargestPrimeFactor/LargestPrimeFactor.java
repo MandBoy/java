6e64c5bf8dc33a9352cc0fc9a07a29db23dcbe30
andreasrmr <andreasrmr@hotmail.com>
2018-10-20 10:55:33 +0200

    .

--- projectEulerChallenge/1-9/3.LargestPrimeFactor/LargestPrimeFactor.java
+++ projectEulerChallenge/1-9/3.LargestPrimeFactor/LargestPrimeFactor.java
@@ -5,24 +5,31 @@ public class LargestPrimeFactor {
    //start tid
    long lStartTime = System.currentTimeMillis();
    //------------------------------------------
-   
+   //--Plan. 
+   //1. Lav liste med tal der går op testnummeret.    
+   //2. Træk det højeste tal fra der går op i et mindre tal - lav ny liste med de nye tal.
 
    int testNumber = 13195;
    
    ArrayList<Integer> list = new ArrayList<Integer>();
+   ArrayList<Integer> primeList = new ArrayList<Integer>();
    
    boolean running = true;
    
    while (running){
-      
+      //1.
       for(int i = 2; i < testNumber; i++){
-         
-         
-         
+         if(testNumber % i == 0){
+            list.add(i);
+         }
       }
+      System.out.println(list);
+      System.out.println(5 % 35);
+      System.out.println(35 % 5);      
+      running = false;
    }
 
-   System.out.println(list.toString());
+  // System.out.println(list.toString());
    
   
    
